2bbf87a1480eb5fbccade23232606446e2cf33e3
Rafael Reis <128103158+Rafael90900@users.noreply.github.com>
2023-10-31 21:13:09 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Segurança</title>
+</head>
+<body>
+    <center>
+         <form id="form-add"
+         action = "#" method= "POST">
+         Nome: <input type="text" 
+         name= "nome"><br><br>
+         CPF: <input type="text"
+         name= "cpf"><br><br>
+          Telefone: <input type="text"
+         name= "telefone"><br><br>
+         Login: <input type="text"
+         name="Login"><br><br>
+          Senha: <input type="password" name="senha"><br><br>
+         <input type="submit" name="cadastrar" value="Cadastrar">
+
+            
+         </form>
+    </center>
+</body>
+</html>
